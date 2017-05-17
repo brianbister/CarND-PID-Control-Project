edede0f067d2bff5405392185b20ba3b0148c307
Brian Bister <brianbister@gmail.com>
2017-05-17 13:44:42 -0700

    Clang-format src

--- src/PID.cpp
+++ src/PID.cpp
@@ -2,20 +2,12 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
-}
+void PID::Init(double Kp, double Ki, double Kd) {}
 
-void PID::UpdateError(double cte) {
-}
+void PID::UpdateError(double cte) {}
 
-double PID::TotalError() {
-}
-
+double PID::TotalError() {}
